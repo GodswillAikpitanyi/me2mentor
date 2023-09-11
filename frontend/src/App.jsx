c0137e0d1f3e9d34b723ea2b77e11062b91d5776
Precious <ikpaprecious2@gmail.com>
2023-09-11 16:01:28 +0100

    ft:added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
-
+//import NavBar from './components/NavBar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import NavBar from './components/NavBar'
 function App() {
 
   return (
     <div>
-      <h1 className='font-black'>
-        Hello world!
-      </h1>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+
+      </Routes>
+
     </div>
   )
 }

08d90d3d64e873596747730806d861f9b564c744
igraziella <igraziella2@gmail.com>
2023-09-12 15:34:40 +0100

    modified hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,10 +7,10 @@ const Hero = () => {
 				<p className="pt-8 text-white">Whether you're a mentor looking to inspire the next generation or a mentee seeking guidance, Me2Mentor is your path to success. <br />Join our community today and embark on a journey of growth, learning, and empowerment.</p>
 			<div className="pt-8 flex gap-2 font-bold">
 				<button className="bg-blue-900 hover:bg-yellow-300 rounded p-3 text-white">Become a Mentor</button>
-				<button className="bg-yellow-300 rounded p-3 text-blue-900">Sign up as a Mentee</button>
-			</div>	
+					<button className="bg-yellow-300 rounded p-3 text-blue-900">Sign up as a Mentee</button>
+				</div>	
+				</div>
 			</div>
-		</div>
 	)
 }
 

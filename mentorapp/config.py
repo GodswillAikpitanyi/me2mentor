2a9b7d78ef5a4868d639cf8ccd56f92767242c78
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-09-18 18:58:20 +0100

    database adjustment

--- mentorapp/config.py
+++ mentorapp/config.py
@@ -3,7 +3,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI ='sqlite:///site.db'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp-mail.outlook.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True

a7bc49e4406a499286530430f46348d597bdfabc
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-09-19 20:41:21 +0100

    little changes

--- frontend/src/pages/MenteeRegister.jsx
+++ frontend/src/pages/MenteeRegister.jsx
@@ -74,7 +74,7 @@ const MenteeRegister = () => {
 			}
 
 			try {
-				const response = await fetch('http://localhost:5000/api/register', {
+				const response = await fetch('http://127.0.0.1:5000/register', {
 					method: 'POST',
 					body: formDataToSend,
 				});

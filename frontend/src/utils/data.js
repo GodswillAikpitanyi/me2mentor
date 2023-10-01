6940c8f1342a449056b28f5f5846f2511953f164
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-10-01 12:10:56 +0100

    frontend

---
+++ frontend/src/utils/data.js
@@ -0,0 +1,102 @@
+export const navLinks = [
+	{
+		id: 1,
+		url: '/',
+		name: 'Home'
+	},
+	{
+		id: 2,
+		url: '/about',
+		name: 'About'
+	},
+	{
+		id: 3,
+		url: '/services',
+		name: 'Services'
+	},
+	{
+		id: 4,
+		url: '/blogs',
+		name: 'Blogs'
+	},
+	{
+		id: 5,
+		url: '/contact',
+		name: 'Contact'
+	},
+]
+
+export const topMentors = [
+	{
+		id: 1,
+		name: 'Ikpa Imade',
+		country: 'Canada',
+		field: 'Finance Specialist',
+		category: 'Banking & Finance',
+		url: '/ment6.jpg',
+		experience: '10 years'
+	},
+	{
+		id: 2,
+		name: 'Graziella',
+		country: 'Nigeria',
+		field: 'Psychologist',
+		category: 'Healthcare',
+		url: '/ment2.jpg',
+		experience: '8 years'
+	},
+	{
+		id: 3,
+		name: 'Adeniyi Adejumo',
+		country: 'Canada',
+		field: 'Web development',
+		category: 'Technology & Innovation',
+		url: '/ment5.jpg',
+		experience: '9 years'
+	},
+	{
+		id: 4,
+		name: 'Godswill Aikpitanyi',
+		country: 'Kenya',
+		field: 'Farmer',
+		category: 'Agriculture',
+		url: '/ment3.jpg',
+		experience: '12 years'
+	},
+	{
+		id: 5,
+		name: 'Ikpa Precious',
+		country: 'Canada',
+		field: 'Web Development',
+		category: 'Technology & Innovation',
+		url: '/ment4.jpg',
+		experience: '10 years'
+	},
+	{
+		id: 6,
+		name: 'Georgette Stone',
+		country: 'Germany',
+		field: 'Medicine & Surgery',
+		category: 'Healthcare',
+		url: '/ment1.jpg',
+		experience: '10years'
+	},
+	{
+		id: 7,
+		name: 'Ikpa Precious',
+		country: 'Rwanda',
+		field: 'Web Development',
+		category: 'Tech',
+		url: '/ment1.jpg',
+		experience: '10years'
+	},
+	{
+		id: 8,
+		name: 'Ikpa Precious',
+		country: 'Canada',
+		field: 'Web Development',
+		category: 'Tech',
+		url: '/ment1.jpg',
+		experience: '10years'
+	},
+]
